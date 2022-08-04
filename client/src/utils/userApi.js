4616138210778527b7033c0641b24faa5198c5f8
roorsk <ravishankar7050@gmail.com>
2022-08-04 22:15:37 +0530

    changing baseurl

--- client/src/utils/userApi.js
+++ client/src/utils/userApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 axios.defaults.baseURL="http://localhost:3001"
+axios.defaults.baseURL="https://rootrsk-youtube-player.herokuapp.com"
 export async function getApi(url){
     try {
         const response = await axios({
